5ca94e5b40a9fad227d8fdb545ee1b7109176d93
kaylatans <120764213+kaylatans@users.noreply.github.com>
2023-04-02 16:11:20 -0600

    Update settings.py

--- missingPersonsGroup/missingPersonsGroup/settings.py
+++ missingPersonsGroup/missingPersonsGroup/settings.py
@@ -115,11 +115,16 @@ USE_I18N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
+# Static files (CSS, JavaScript, Images), for implementing Bootstrap files
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'missingPersonsGroup/static')
+]       
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
