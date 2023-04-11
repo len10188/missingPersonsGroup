137a1f23ea3b1245b298ddca3e66b095d1cfa965
mmbates <megan.martine.bates@gmail.com>
2023-04-11 17:15:36 -0600

    Added page 2 and adjusted the views file.

--- missingPersonsGroup/landingPage/urls.py
+++ missingPersonsGroup/landingPage/urls.py
@@ -1,7 +1,12 @@
+'''
+Authors: Megan Bates, Steven Armstrong, Kayla Tansiongco, Laura Nielson, Elise Pickett, Autumn Eaton.
+Purpose: To store the routes for each page in the application.
+'''
 # importing urls and setting up path
 from django.urls import path
-from .views import indexPageView
+from .views import indexPageView, individualPageView
 
 urlpatterns = [
-    path("", indexPageView, name = "index")
+    path("", indexPageView, name = "index"),
+    path("individual", individualPageView, name = "individual")
 ]
