355e6316540cdf47fd5b9df05cc3f1f1cc619363
Megan Bates <121825794+mmbates@users.noreply.github.com>
2023-04-11 21:43:07 -0500

    Update models.py

--- missingPersonsGroup/landingPage/models.py
+++ missingPersonsGroup/landingPage/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class Person(models.Model):
+  date_missing = models.DateField()
+  last_name = models.CharField(max_length=30)
+  first_name = models.CharField(max_length=30)
+  age_at_missing = models.IntegerField()
+  city = models.CharField(max_length=30)
+  state = models.CharField(max_length=20)
+  gender = models.CharField(max_length=1)
+  race = models.CharField(max_length=1)
+  
+  def __str__(self):
+    return '%d %s %s %i %s %s %s %s' % (self.date_missing, self.last_name, self.first_name,
+                      self.age_at_missing, self.city, self.state, self.gender, self.race)
