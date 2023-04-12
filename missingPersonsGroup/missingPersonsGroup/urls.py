0f804496803c77d400de035c475e6ea70bcf7ef0
Megan Bates <121825794+mmbates@users.noreply.github.com>
2023-04-12 09:02:47 -0600

    Update urls.py

--- missingPersonsGroup/missingPersonsGroup/urls.py
+++ missingPersonsGroup/missingPersonsGroup/urls.py
@@ -13,15 +13,14 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-
 from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
-    path("", include('landingPage.urls')),
-    path("home", include('landingPage.urls')),
-    path("individual", include('landingPage.urls')),
-    path("resources", include('landingPage.urls')),
-    path("database", include('landingPage.urls')),
+    path('admin/', admin.site.urls),
+    path('', include('landingPage.urls')),
+    path('home', include('landingPage.urls')),
+    path('individual', include('landingPage.urls')),
+    path('resources', include('landingPage.urls')),
+    path('database', include('landingPage.urls')),
 ]
