6b42bf9816068992073d535e0d5360ad174b17dc
Lauraenielson <122550044+Lauraenielson@users.noreply.github.com>
2023-04-18 10:27:38 -0600

    added comments to views.py

--- missingPersonsGroup/missingPersonsGroup/settings.py
+++ missingPersonsGroup/missingPersonsGroup/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'missing_persons',
         'USER': 'postgres',
-        'PASSWORD': 'Brisingr1',
+        'PASSWORD': '',
         'HOST': 'localhost'
     }
 }
