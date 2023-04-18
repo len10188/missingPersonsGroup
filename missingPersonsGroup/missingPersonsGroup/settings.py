badf91a89381b88ebec3977d055638c17b6763ea
elisepickett <pickettelise0@gmail.com>
2023-04-18 17:54:36 -0600

    static changes

--- missingPersonsGroup/missingPersonsGroup/settings.py
+++ missingPersonsGroup/missingPersonsGroup/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'missing_persons',
         'USER': 'postgres',
-        'PASSWORD': 'danielle1',
+        'PASSWORD': 'admin',
         'HOST': 'localhost'
     }
 }
